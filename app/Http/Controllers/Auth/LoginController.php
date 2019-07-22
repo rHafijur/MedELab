9ac3c56267902600dbb641ceebd45aed730ecdd0
rhafijur <bmsakib09.hr@yahoo.com>
2019-07-22 16:23:15 +0600

    auth redirect path has been changed to dynamic path

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -25,7 +26,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    // protected $redirectTo = '/superadmin';
 
     /**
      * Create a new controller instance.
@@ -33,7 +34,40 @@ class LoginController extends Controller
      * @return void
      */
     public function __construct()
-    {
+    {   
         $this->middleware('guest')->except('logout');
+    }
+    protected function redirectTo(){
+        if(Auth::check()){
+            switch (Auth::user()->role_id) {
+                case 1:
+                return '/superadmin';
+                break;
+
+            case 2:
+                return '/patient';
+                break;
+            
+            case 3:
+                // return '/doctor';
+                break;
+
+            case 4:
+                return '/word_admin';
+                break;
+            case 5:
+                return '/pharmacy_admin';
+                break;
+            case 6:
+                return '/counter_admin';
+                break;
+            case 7:
+                // return '/lab_admin';
+                break;
+            default:
+                return abort(404);
+                break;
+            }
+        }
     }
 }
